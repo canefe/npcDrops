641b20bcb6d03516276d950e3c32ece74448fe71
canefe <turkiyecraft@gmail.com>
2017-11-18 19:30:50 +0300

    1.5.1
    * Removed some junks.

--- npcDrops/lua/autorun/sh_loadnpcdrops.lua
+++ npcDrops/lua/autorun/sh_loadnpcdrops.lua
@@ -37,7 +37,7 @@ end
 for k, i in ipairs( ydiag ) do 
     MsgC( Color( 255, 255, 0 ), i .. '\n\n' )
 end
-local rgb=Color
+
 if CLIENT then
 	local files = file.Find( "npcdrops/*", "LUA" )
 	for _,v in pairs(files) do
@@ -51,99 +51,8 @@ if CLIENT then
 		end
 	end	
 
-		net.Receive("testerx", function()
-
-		local killer = net.ReadString()
-
-		local frame = vgui.Create("questionFlat")
 
 
-
-		frame:SetSize(300, 200)
-		frame:Center()
-		frame:SetTitle("RDM - Checker")
-
-
-		local richtext = vgui.Create( "RichText", frame )
-		richtext:Dock( TOP )
-		richtext:SetTall(100)
-
-		richtext:SetText( "Bu bir RDM miydi?" )
-		richtext:SetVerticalScrollbarEnabled( false )
-		richtext:SetMouseInputEnabled(false)
-
-		local KillerText = vgui.Create( "DLabel" , frame )
-		KillerText:SetFont("cool30")
-		KillerText:SetText("Katil: "..(killer or "NaN"))
-		KillerText:SizeToContents()
-		KillerText:SetPos(20,70)
-
-		local Y = vgui.Create( "DButton", frame )
-		Y:SetText( "Evet" )
-		Y:SetTall(40)
-		Y:SetWide(70)
-		Y:SetPos( 20, 120 )
-		Y:SetTooltip( "RDM ise buna bas." )
-		Y:SetFont("cool18")
-		Y:SetTextColor(Color(255,255,255))
-		Y.Paint = function(s, w, h)
-				if s:IsHovered() then 
-					draw.RoundedBox(0,0,0,w,h,rgb(231, 76, 60))
-				else
-					draw.RoundedBox(0,0,0,w,h,rgb(192, 57, 43))
-				end
-		end
-
-		Y.DoClick = function()
-
-			frame:Close()
-
-		end
-
-
-		local N = vgui.Create( "DButton", frame )
-		N:SetText( "Hayır" )
-		N:SetTall(40)
-		N:SetWide(70)
-		N:SetPos( 120, 120 )
-		N:SetTooltip( "RDM değilse buna bas." )
-		N:SetFont("cool18")
-		N:SetTextColor(Color(255,255,255))
-		N.Paint = function(s, w, h)
-				if s:IsHovered() then 
-					draw.RoundedBox(0,0,0,w,h,rgb(46, 204, 113))
-				else
-					draw.RoundedBox(0,0,0,w,h,rgb(39, 174, 96))
-				end
-		end	
-
-		N.DoClick = function()
-
-			frame:Close()
-
-		end
-
-
-
-		function richtext:PerformLayout()
-
-			self:SetFontInternal( "cool30" )
-			--self:SetBGColor( rgb(44, 62, 80, 200) )
-			self:SetFGColor( Color( 255, 255, 255 ) )
-
-
-		end
-
-		timer.Simple(6 ,function()
-
-
-			frame:Close()
-		end)
-
-
-
-
-	end)
 
 end
 
@@ -164,46 +73,6 @@ if SERVER then
 	end
 
 
-	util.AddNetworkString("testerx")
-
-	concommand.Add("testerd", function(ply,cmd,arg)
-
-		net.Start("testerx")
-
-		net.Send(ply)
-
-
-	end)
-
-	hook.Add("PlayerDeath","rdm_Check", function(ply,inf,att)
-
-
-		if IsValid(ply) and IsValid(att) and att:IsPlayer() and ply:IsPlayer() then
-
-
-		elseif IsValid(ply) and IsValid(att) and ply:IsPlayer() then
-
-			if att:IsVehicle() then
-
-				att = att:GetDriver()
-
-			end
-
-		end
-
-		if att == ply then return end
-
-		net.Start("testerx")
-
-			net.WriteString(att:Name())
-
-		net.Send(ply)
-
-
-
-
-
-	end)
 
 end
 
